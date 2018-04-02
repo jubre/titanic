85d507b7934de89b3eb1153c571d670956450566
Junior Corman <junior.corman@gmail.com>
2018-04-01 21:36:01 -0500

    Read data, describing data

--- titanic.py
+++ titanic.py
@@ -8,3 +8,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 #%matplotlib inline
+
+
+train_df = pd.read_csv('~/.kaggle/competitions/titanic/train.csv')
+test_df = pd.read_csv('~/.kaggle/competitions/titanic/test.csv')
+combine = [train_df, test_df]
+
+# Describiendo la data
+print(train_df.columns.values)
+
+# Previsualizacion
+print(train_df.head())
