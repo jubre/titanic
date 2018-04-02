012cbff74f2564de3d20edd4365ddea5a412f6f9
Junior Corman <junior.corman@gmail.com>
2018-04-01 21:11:30 -0500

    Add library support

---
+++ titanic.py
@@ -0,0 +1,10 @@
+# Analisis de datos y Manejo
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# Visualizacion
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
