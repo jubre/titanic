d355ca5ced90fc900ec46adca2e0966bcae7e0de
Junior Corman <junior.corman@gmail.com>
2018-04-02 02:35:25 -0500

    Stochastic Gradient Descent

--- titanic.py
+++ titanic.py
@@ -251,3 +251,10 @@ linear_svc.fit(X_train, Y_train)
 Y_pred = linear_svc.predict(X_test)
 acc_linear_svc = round(linear_svc.score(X_train, Y_train) * 100, 2)
 acc_linear_svc
+
+# Stochastic Gradient Descent
+sgd = SGDClassifier()
+sgd.fit(X_train, Y_train)
+Y_pred = sgd.predict(X_test)
+acc_sgd = round(sgd.score(X_train, Y_train) * 100, 2)
+acc_sgd
