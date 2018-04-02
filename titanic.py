f20d8a6a25c2f2c6d3f9e97bbfdb14e24ead9cc2
Junior Corman <junior.corman@gmail.com>
2018-04-01 22:53:33 -0500

    Analizando mediante Pivote de caracteristicas

--- titanic.py
+++ titanic.py
@@ -26,7 +26,12 @@ train_df.info()
 print('_'*40)
 test_df.info()
 
-
 # Descriniendo los datos
 print(train_df.describe())
 print(train_df.describe(include=['O']))
+
+# Analizando mediante Pivote de caracteristicas
+print(train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+print(train_df[["Parch", "Survived"]].groupby(['Parch'], as_index=False).mean().sort_values(by='Survived', ascending=False))
